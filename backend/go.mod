5742262ad4bfcbccfb9bab4e7d2ca446859f788b
Yuvraj2305 <yuvrajykarmarkar23@gmail.com>
2026-05-22 20:36:50 +0530

    task 3 complete

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module secureflow
 
 go 1.26.3
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+)
--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -1,6 +1,21 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 const(
 	RoleAdmin = "admin"
 	RoleAnalyst = "analyst"
 )
+
+type User struct{
+	ID uuid.UUID  `json:"id" db:"id"`
+	Email string `json:"email" db:"email"`
+	PasswordHash string `json:"password_hash" db:"password_hash"`
+	Role string `json:"role" db:"role"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	Updatedat time.Time `json:"updated_at" db:"updated_at"`
+}
